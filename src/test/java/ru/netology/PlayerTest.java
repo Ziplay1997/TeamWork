1b9867d0b536e21011179b3b5dd0a8a6401f5668
Vladislav Chudaev <vladislav.chudaev1998@gmail.com>
2023-02-27 22:33:23 +0300

    add picture2

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -19,5 +19,26 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    // новые тесты
+
+    @Test
+    public void TestSumGenreTwoGames() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Call of Duty", "Battle Royale");
+        Game game1 = store.publishGame("Uncharted", "Battle Royale");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.installGame(game3);
+        player.play(game, 3);
+        player.play(game1, 2);
+        player.play(game3, 1);
+        player.play(game1, 1);
+
+        int expected = 3;
+        int actual = player.sumGenre("Battle Royale");
+        assertEquals(expected, actual);
+    }
 }
